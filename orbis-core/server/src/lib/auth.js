27ea0d66795816ca9eeec7cdab99d2eac5d83cce
Bishwajit Pattanaik <bishwajit.pattanaik2004@gmail.com>
2026-06-30 02:40:11 +0530

    fixed deployment issues

--- orbis-core/server/src/lib/auth.js
+++ orbis-core/server/src/lib/auth.js
@@ -5,6 +5,11 @@ import { deviceAuthorization } from "better-auth/plugins";
 
 
 
+import {betterAuth} from "better-auth";
+import {prismaAdapter} from "better-auth/adapters/prisma";
+import prisma from "./db.js";
+import { deviceAuthorization } from "better-auth/plugins";
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
@@ -15,6 +20,12 @@ export const auth = betterAuth({
         "http://localhost:3000",
         "https://orbis-ai-bishwajitpattanaik.vercel.app",
     ],
+    advanced: {
+        defaultCookieAttributes: {
+            sameSite: "none",
+            secure: true,
+        },
+    },
     plugins: [
         deviceAuthorization({ 
             verificationUri: "/device", 
