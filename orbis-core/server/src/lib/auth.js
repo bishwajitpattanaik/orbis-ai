759d1d21e11e4dbc9dd18cdeccad3a8a0c46f218
Bishwajit Pattanaik <bishwajit.pattanaik2004@gmail.com>
2026-06-30 02:45:50 +0530

    fixed deployment issues

--- orbis-core/server/src/lib/auth.js
+++ orbis-core/server/src/lib/auth.js
@@ -3,13 +3,6 @@ import {prismaAdapter} from "better-auth/adapters/prisma";
 import prisma from "./db.js";
 import { deviceAuthorization } from "better-auth/plugins";
 
-
-
-import {betterAuth} from "better-auth";
-import {prismaAdapter} from "better-auth/adapters/prisma";
-import prisma from "./db.js";
-import { deviceAuthorization } from "better-auth/plugins";
-
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
